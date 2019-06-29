5e96df22b8b54b3360c0ccd06b3954c64b8e84c4
Deep Sehgal <dmsehgal87@gmail.com>
2019-06-29 00:28:45 -0400

    Added steps and Date

--- app/index.js
+++ app/index.js
@@ -3,22 +3,48 @@ import document from "document";
 import { preferences } from "user-settings";
 import * as util from "../common/utils";
 import * as messaging from "messaging";
+import userActivity from "user-activity";
 import { me } from "companion"
 
 
 // Update the clock every minute
-clock.granularity = "seconds";
+clock.granularity = "minutes";
 
 // Get a handle on the <text> element
 const myLabel = document.getElementById("myLabel");
 const matchLabel = document.getElementById("matchLabel");
+const date = document.getElementById("date");
 const score1 = document.getElementById("score1");
 const score2 = document.getElementById("score2");
 const result = document.getElementById("result");
+var month = new Array();
+  month[0] = "Jan";
+  month[1] = "Feb";
+  month[2] = "Mar";
+  month[3] = "Apr";
+  month[4] = "May";
+  month[5] = "Jun";  
+  month[6] = "Jul";
+  month[7] = "Aug";
+  month[8] = "Sep";
+  month[9] = "Oct";
+  month[10] = "Nov";
+  month[11] = "Dec";
+
+var day = new Array();
+day[0]= "Sun";
+day[1]= "Mon";
+day[2]= "Tue";
+day[3]= "Wed";
+day[4]= "Thu";
+day[5]= "Fri";
+day[6]= "Sat";
 
 // Update the <text> element every tick with the current time
 clock.ontick = (evt) => {
   let today = evt.date;
+  date.text = day[today.getDay()] +", "+month[today.getMonth()]+" "+today.getDate();
+  //console.log("day:"+day[today.getDay()]+"date:"+today.getDate()+" month:"+month[today.getMonth()]);
   let hours = today.getHours();
   if (preferences.clockDisplay === "12h") {
     // 12h format
@@ -31,8 +57,9 @@ clock.ontick = (evt) => {
   //console.log(myLabel.toString());
   myLabel.text = `${hours}:${mins}`;
   
+  
   //get the match data
-  var url = 'https://cricketapi-icc.pulselive.com/fixtures?matchTypes=T20I%2CT20%2CTEST%2CODI%2CFIRST_CLASS%2CLIST_A&tournamentTypes=I%2CWI&teamTypes=m&matchStates=C&page=0&pageSize=10&sort=desc';
+  var url = 'https://cricketapi-icc.pulselive.com/fixtures?matchTypes=T20I%2CT20%2CTEST%2CODI%2CFIRST_CLASS%2CLIST_A&tournamentTypes=I%2CWI&teamTypes=m&matchStates=C&page=0&pageSize=2&sort=desc';
   var data = {'key' : 'val'}
   if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
           //Send object as JSON string to companion
@@ -59,6 +86,14 @@ clock.ontick = (evt) => {
     result.text = obj.matchStatus;
     
   }
+  
+  //update the steps and date
+  const metricSteps = "steps";
+  const amountSteps = userActivity.today.adjusted[metricSteps] || 0;
+  var steps = document.getElementById("steps");
+  //console.log(amountSteps);
+  steps.text = amountSteps;
+  
   
 }
 
