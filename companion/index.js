6cafdb266badf8bb870aa38c621145acd64e08ba
Deep Sehgal <dmsehgal87@gmail.com>
2019-06-29 00:30:10 -0400

    Updated for correct url and close console logs

--- companion/index.js
+++ companion/index.js
@@ -1,10 +1,16 @@
 import * as messaging from "messaging";
 import { me } from "companion"
 
-var url = 'https://cricketapi-icc.pulselive.com/fixtures?matchTypes=T20I%2CT20%2CTEST%2CODI%2CFIRST_CLASS%2CLIST_A&tournamentTypes=I%2CWI&teamTypes=m&matchStates=C&page=0&pageSize=10&sort=desc';
+var url_old1 = 'https://cricketapi-icc.pulselive.com/fixtures?matchTypes=T20I%2CT20%2CTEST%2CODI%2CFIRST_CLASS%2CLIST_A&tournamentTypes=I%2CWI&teamTypes=m&matchStates=C&page=0&pageSize=10&sort=desc';
+var old='https://cricketapi-icc.pulselive.com/fixtures?matchTypes=ODI&tournamentTypes=I%2CWI&teamTypes=m&matchStates=C&page=0&pageSize=10&sort=desc&tournamentLabels=ICC%20Cricket%20World%20Cup%202019';
 const host = "https://bz1ikefgtf.execute-api.us-east-1.amazonaws.com/api/";
+var url='https://www.cricbuzz.com/api/html/homepage-scag';
 
 console.log("Companion Running ");
+
+function getElementByXpath(path, data) {
+  return document.evaluate(path, data, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
 
 //Server where the API is runnong (must be HTTPS)
 
@@ -15,21 +21,32 @@ myHeaders.append('Content-Type', 'application/json');
 
 if (me.launchReasons.peerAppLaunched) {
   // The Device application caused the Companion to start
-  console.log("Device application was launched!")
+  //console.log("Device application was launched!")
 }
 
 //When the watch sends a message
 messaging.peerSocket.onmessage = evt => {
   //console.log("Data recieved: " + evt.data); //Log it
   //var url = host + "/fitbit"; // add a path to the URL
-  fetch(url, {
+  fetch(url_old1, {
       method : "GET",
       //headers : myHeaders,
       /*body: evt.data*/}) // Build the request
     .then(function(response){
-      return response.json();}) //Extract JSON from the response
-    .then(function(data) {             
-      //console.log("Got response from server:", JSON.stringify(data)); // Log ig
+      //return response.text();}) //Extract HTML from the response
+    return response.json();}) //Extract JSON from the response
+    .then(function(data) {   
+    //var oParser = new DOMParser();
+    //var xmlDoc = oParser.parseFromString(data, "text/html");
+    // print the name of the root element or error message
+    //console.log(xmlDoc.documentElement.nodeName == "parsererror" ? "error while parsing" : xmlDoc.documentElement.nodeName);
+    //var test=oDOM.documentElement.getAttribute("lang");
+    //var test=xmlDoc.documentElement.getElementsById("hm-scag-mtch-blk").getAttribute("class");
+    //console.log(test);
+    //console.log("DOM");
+    //console.log("TEST");
+    //console.log("Got response from server:", data); // Log ig
+    //console.log("Got response from server:", JSON.stringify(data)); // Log ig
     var team1 = data.content[0].scheduleEntry.team1.team.abbreviation;
     var team2 = data.content[0].scheduleEntry.team2.team.abbreviation;
     var teams = team1 +" VS "+team2;
